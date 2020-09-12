ef8731344504694677bc0d6baf781c0a1a9e7333
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-09-12 21:01:09 +0100

    Update status.js

--- IntervalStatus/status.js
+++ IntervalStatus/status.js
@@ -1,7 +1,7 @@
 module.exports = function({ bot, config, commands }) {
   Const fs = require('fs')
   Const ini = require('ini')
-  Const sconfig = ini.parse(fs.readFileSync('./IntervalStatus/config.ini', 'utf-8'))
+  Const sconfig = ini.parse(fs.readFileSync('./ModmailPlugins/IntervalStatus/status.config.ini', 'utf-8'))
   Const msg = require(sconfig.status)
   Const prefix = require(config.prefix)
   
