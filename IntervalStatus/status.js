da5f71c43428baccbb59fb4183044eabb2a14504
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-10-03 15:01:25 +0100

    Delete status.js

--- IntervalStatus/status.js
+++
@@ -1,9 +0,0 @@
-module.exports = function({ bot, config, commands }) {
-  Const fs = require('fs')
-  Const ini = require('ini')
-  Const sconfig = ini.parse(fs.readFileSync('./ModmailPlugins/IntervalStatus/status.config.ini', 'utf-8'))
-  Const msg = require(sconfig.status)
-  Const prefix = require(config.prefix)
-  
-  
-}
